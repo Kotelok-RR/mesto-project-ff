dc96677a829d271ce6ca4c71093d98d063fb9ffd
Kotelok-RR <petrov16052006@gmail.com>
2025-02-10 18:34:03 +0300

    feat: realization of card creation from temlate, add card deletion functionality, render initial cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,58 @@
-// @todo: Темплейт карточки
+const page = document.querySelector('.page')
+const cardAddButton = page.querySelector('.profile__add-button');
+const newCardPopupContainer = page.querySelector('.popup_type_new-card');
+const newCardCloseButton = newCardPopupContainer.querySelector('.popup__close');
+const cardsList = page.querySelector('.places__list');
+const newCardSaveButton = newCardPopupContainer.querySelector('.popup__button');
 
-// @todo: DOM узлы
+const closePopup = (popupWindow) => {
+    popupWindow.removeAttribute('style');
+}
 
-// @todo: Функция создания карточки
+cardAddButton.addEventListener('click', () => {
+    newCardPopupContainer.setAttribute('style', 'display: flex');
+});
 
-// @todo: Функция удаления карточки
+newCardCloseButton.addEventListener('click', () => {
+    closePopup(newCardPopupContainer);
+});
 
-// @todo: Вывести карточки на страницу
+const deleteCard = (e) => {
+    const card = e.target.closest('.card');
+    card.remove();
+}
+
+const createCard = (cardImage, cardTitle, deleteCard) => {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const newCard = cardTemplate.querySelector('.card').cloneNode(true);
+
+    newCard.querySelector('.card__title').textContent = cardTitle;
+    newCard.querySelector('.card__image').setAttribute('src', cardImage);
+    
+    const deleteCardButton = newCard.querySelector('.card__delete-button');
+    deleteCardButton.addEventListener('click', deleteCard)
+
+    return newCard;
+}
+
+newCardSaveButton.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const imageLinkContainer = newCardPopupContainer.querySelector('.popup__input_type_url');
+    const titleContainer = newCardPopupContainer.querySelector('.popup__input_type_card-name');
+
+    if (imageLinkContainer.value && titleContainer.value) {
+        const newCardElement = createCard(imageLinkContainer.value, titleContainer.value, deleteCard);
+        cardsList.append(newCardElement);
+        
+        imageLinkContainer.value = '';
+        titleContainer.value = '';
+
+        closePopup(newCardPopupContainer);
+    }
+})
+
+initialCards.forEach(card => {
+    const cardElement = createCard(card.link, card.name, deleteCard);
+    cardsList.append(cardElement);
+})
